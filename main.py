1ef7e9c7eb7de050c4708011959a78f387218a81
yantimirov-timur <tyantimirov@yandex.ru>
2021-03-28 14:01:16 +0300

    hello its me

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 
-
-
 if __name__ == '__main__':
-    dictionary = [0,1,1,2,3,4,5,6]
+    dictionary = [0, 1, 1, 2, 3, 4, 5, 6]
     s = pd.Series(dictionary)
     s.plot.kde()
 
-
     f = 0.5
     print(1)
+    print("hello its me")
